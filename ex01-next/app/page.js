1af41fcdf3785f53db724517de2449f511091d3d
vasengprod <vinicius.asilva@ufpe.br>
2025-08-19 00:24:59 +0000

    Conversão htlm para jsx

--- ex01-next/app/page.js
+++ ex01-next/app/page.js
@@ -1,6 +1,17 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import {Profile} from '@/componente/Profile';
+import {TodoList} from '@/componente/TodoList';
+import {Avatar} from '@/componente/Avatar';
+
 export default function Home(){
-  return <div>Olá Turma!</div>;
+  return (
+  <div>
+    <h1>Olá turma!</h1>
+    <Profile />
+    <TodoList />
+    <Avatar />
+  </div>
+  );
 }
