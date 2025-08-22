28527f373d5357b5a70bf7871736781a3aa95cd0
vasengprod <vinicius.asilva@ufpe.br>
2025-08-22 00:44:48 +0000

    Atividade concluida

---
+++ atividade01/app/page.js
@@ -0,0 +1,13 @@
+
+import {Profile} from '@/componentes/Profile';
+
+export default function Home(){
+  return (
+  <div>
+    <h1>Site criado para disciplina de programação front-end</h1>
+    <Profile />
+  </div>
+  );
+}
+
+
---
+++ atividade01/componentes/Profile.js
@@ -0,0 +1,14 @@
+export function Profile() {
+  return (
+    <div className="container">
+      <div className="profile-card">
+        <img src="/WhatsApp Image 2025-08-20 at 01.13.39.jpeg" alt="Minha foto" />
+        <div className="profile-info">
+          <h2>Vinícius de Almeida Silva</h2>
+          <p>Engenheiro de Produção e estudante de SI, tenho interesse na área de dados aplicado a negócios.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
